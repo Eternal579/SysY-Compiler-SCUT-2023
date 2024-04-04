326855f097606cdb8b1c496854665305e120c0db
MinXin <98520126+Eternal579@users.noreply.github.com>
2024-04-04 23:46:03 +0800

    Delete Parse directory

--- Parse/util.h
+++
@@ -1,9 +0,0 @@
-	#ifndef _UTIL_H
-#define _UTIL_H
-
-#include"global.h"
-/* Procedure printToken prints a token
-ans its lexme to the listibng file */
-void printToken(TokenType, const char*);
-
-#endif
